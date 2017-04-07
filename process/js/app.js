f309439e3c1a3c64ed6b305739a72a43800b5eae
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-04-07 10:17:34 -0400

    Completed

--- process/js/app.js
+++ process/js/app.js
@@ -4,32 +4,50 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
-      title: 'Appointments',
-      show: true
+
+        myApointments : []
+
     } //return
   }, //getInitialState
+      componentDidMount: function(){
 
+           this.serverRequest = $.get('./js/data.json', function(result){
+              var tempArrPets = result;
+              this.setState({
+                myApointments: tempArrPets
+              }); //set state
+          }.bind(this));
+      },
+      componentWillUnmount: function(){
+        this.serverRequest.abort();
+      },
   render: function() {
-    var showTitle;
-    if (this.state.show) {
-      showTitle = 'New';
-    }
+          var filteredApt = this.state.myApointments;
+          filteredApt= filteredApt.map(function(item,index){
+              return (
+                
+                            <li className ="pet-item media" key = {index}> 
+                            <div className ="pet-info media-body">
+                            <div className= "pet-head">
+                            <span className="pet-name">{this.state.myApointments[index].petName}</span>
+                            <span className ="apt-date pull-right">{this.state.myApointments[index].aptDate}</span>
+                          </div>
+                         <div className="owner-name">
+                         <span className="label-item">Owner:</span>{this.state.myApointments[index].ownerName}
+                         <div className="apt-notes">{this.state.myApointments[index].aptNotes}</div>
+      
+                         </div> 
+                        </div>
+                      </li>
+              ) //rendering data as HTML fuction     
 
-    var displayList = {
-      display: this.state.show ? 'block':'none',
-      color: 'red'
-    }
+          }.bind(this)); //filtering an array of data! 
 
     return (
-      <div className="interface">
-        <h1>{ showTitle } {this.state.title}</h1>
-        <ul style={displayList}>
-          <li>Buffy 3:30 PM</li>
-          <li>Spot 8:30 PM</li>
-          <li>Goldie 3:50 PM</li>
-        </ul>
-      </div>
-    )
+            <div className="interface">
+                  <ul className="item-list media-list">{filteredApt}</ul>
+            </div>
+    )//return
   } //render
 }); //MainInterface
 
